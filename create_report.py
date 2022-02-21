02678bb709d8d97910d4cc723c0b9247aa6c0b9b
Sean Geary <sgeary@flexera.com>
2022-02-21 06:45:28 -0500

    Fix indentation issue

--- create_report.py
+++ create_report.py
@@ -88,13 +88,13 @@ def main():
 			logger.error("Unable to open properties file: %s" %propertiesFile)
 
 		# Is there a self signed certificate to consider?
-        try:
-            certificatePath = configData["core.server.certificate"]
-            os.environ["REQUESTS_CA_BUNDLE"] = certificatePath
-            os.environ["SSL_CERT_FILE"] = certificatePath
-            logger.info("Self signed certificate added to env")
-        except:
-            logger.info("No self signed certificate in properties file")
+		try:
+			certificatePath = configData["core.server.certificate"]
+			os.environ["REQUESTS_CA_BUNDLE"] = certificatePath
+			os.environ["SSL_CERT_FILE"] = certificatePath
+			logger.info("Self signed certificate added to env")
+		except:
+			logger.info("No self signed certificate in properties file")
 			
 	else:
 		baseURL = "http://localhost:8888"   # Required if the core.server.properties files is not used
